6d8de95c72f6aa2a30decf8f1070914831aa51d2
KouIshihara <as.it.were.kou@gmail.com>
2018-11-03 14:32:48 +0900

    Sprint2

---
+++ public/scripts/customVars.js
@@ -0,0 +1,46 @@
+var profileVars = [
+  {
+    id: 'nickname',
+    label: 'ニックネーム',
+  },
+  {
+    id: 'hobby',
+    label: '趣味',
+  },
+  {
+    id: 'my_supecial_performance',
+    label: 'すごい一芸',
+  },
+  {
+    id: 'my_havitual_saying',
+    label: '口ぐせ',
+  },
+  {
+    id: 'resemble_entertainer',
+    label: '似ている芸能人',
+  },
+  {
+    id: 'resemble_charactor',
+    label: '似ているキャラクター',
+  },
+  {
+    id: 'recent_event',
+    label: '最近の出来事',
+  },
+  {
+    id: 'my_goal',
+    label: '目標',
+  },
+  {
+    id: 'my_master',
+    label: '人生の師',
+  },
+  {
+    id: 'my_passion_in_youth',
+    label: '学生時代の取り組み',
+  },
+  {
+    id: 'my_boom',
+    label: 'マイブーム',
+  },
+];
--- public/scripts/router.js
+++ public/scripts/router.js
@@ -6,11 +6,116 @@ riot.route('/test/*', function(tagName) {
   riot.update();
 })
 
+
 riot.route('/users/*', function(uid) {
   riot.mount('header', 'util-header', {status: 'back'});
   riot.mount('modal-content', 'page-user-account', {content: uid});
   riot.update();
 })
+
+
+riot.route('/pre-input-profile/*', function(uid) {
+  riot.toNextFunction = function(){
+    window.location.href = `./#input-profile/${riot.focusedUid}`;
+  };
+  riot.mount('header', 'util-header', {
+    status: 'back_and_next',
+    uid: uid,
+    label: '項目を選択'
+  });
+  riot.mount('modal-content', 'page-pre-input-profile', {content: uid});
+  riot.update();
+})
+
+
+riot.route('/input-profile/*', function(uid) {
+  
+  //if(true)
+  riot.toSaveFunction = async function(){
+
+    if(riot.focusedUid == "new_person"){
+      var invitation = await service.db.collection("invitaions").add({
+        name: riot.nameToInvite,
+        from: session.user.uid,
+        createdAt: new Date(),
+      })
+      var invitationId = invitation.id;
+      riot.invitationId = invitationId;
+      var profiles = riot.toSaveProfiles;
+      riot.toSaveProfiles = profiles.map(function(e){
+        e.invitationId = invitationId;
+        return e;
+      })
+    }
+
+    //  TO DO: 一括書き込みができるbatchをつかおう
+    riot.savedCounter = 0;
+    $('#loadingForModal').fadeIn(400);
+    for(var i=0; i<riot.toSaveProfiles.length; i++){
+      var profileObj = riot.toSaveProfiles[i];
+      service.db.collection("profiles")
+      .add(profileObj)
+      .then(function(){
+        riot.savedCounter++;
+        if(riot.savedCounter == riot.toSaveProfiles.length){
+          console.log('saveComp');
+          riot.toSaveProfiles = [];
+          riot.savedCounter = 0;
+          $('#loadingForModal').fadeOut(400);
+          if(riot.focusedUid != "new_person"){
+            window.history.go(-2);
+          }else{
+            window.location.href = `./#complete-invitation/${riot.invitationId}`;
+          }
+        }
+      });
+    } // for
+
+    // こちらのプロフィールを書いてくれていてpublishされていないprofileを有効にする
+    var profileByFromUser = await service.db.collection("profiles")
+      .where("from", "==", riot.focusedUid)
+      .where("to", "==", session.user.uid)
+      .where("publish", "==", false)
+      .get().then(function(querySnapshot) {
+        var profiles = [];
+        querySnapshot.forEach(function(doc) {
+          profiles.push({
+            data: doc.data(),
+            id: doc.id,
+          })
+        });
+        return profiles;
+      });
+
+    for(var i=0; i<profileByFromUser.length; i++){
+      var doc = profileByFromUser[i];
+      service.db.collection("profiles").doc(doc.id).update({publish: true});
+    }
+
+    // TO DO: fromからプロフィールを書き込んでもらっていたかどうかによる、いずれかの通知を送る
+  }; // toSaveFunction()
+
+  riot.mount('header', 'util-header', {
+    status: 'back_and_save',
+    uid: uid,
+    label: '項目を選択'
+  });
+  riot.mount('modal-content', 'page-input-profile', {content: uid});
+  riot.update();
+})
+
+riot.route('/complete-invitation/*', function(invitationId) {
+  riot.toCompleteFunction = function(){
+    window.location.href = `./#invite-user`;
+  };
+  riot.mount('header', 'util-header', {
+    status: 'complete',
+  });
+  riot.mount('modal-content', 'page-complete-invitation', {invitationId: invitationId});
+  riot.update();
+})
+
+
 
 riot.route('/edit-profile', function() {
 
@@ -29,7 +134,7 @@ riot.route('/edit-profile', function() {
 })
 
 riot.route('/login', function(tagName) {
-
+  
   if(riot.enableFadeIn) $('content').removeClass('not-opacity');
 
   riot.enableFadeIn = true;
@@ -49,7 +154,10 @@ riot.route('/user-list', function(tagName) {
 
   riot.enableFadeIn = true;
 
-  riot.mount('header', 'util-header', {status: 'normal'});
+  riot.mount('header', 'util-header', {
+    status: 'normal',
+    label: 'リスト'
+  });
   riot.update();
 
   $(document).trigger("custom:close");
@@ -60,26 +168,56 @@ riot.route('/user-list', function(tagName) {
       riot.mount('content', 'page-user-list', {content: 'content'});
       riot.update();
     }, 400);
-  //}else{
-    riot.enableReloadContent = true;
   //}
+  riot.enableReloadContent = true;
+  
 })
 
-riot.route(function(tagName) {
-  window.location.href = './#login';
-  /*
+riot.route('/invite-user', function(tagName) {
+
   if(riot.enableFadeIn) $('content').removeClass('not-opacity');
 
   riot.enableFadeIn = true;
 
-  $(document).trigger("custom:close");
+  riot.toNextFunction = function(){
+    window.location.href = `./#pre-input-profile/new_invitation`;
+  };
+  riot.mount('header', 'util-header', {
+    status: 'next',
+    label: '招待'
+  });
+  riot.update();
 
-  setTimeout(function() {
-    $('content').addClass('not-opacity');
-    riot.mount('content', 'page-login', {content: 'content'});
-    riot.update();
-  }, 400);
-  */
+  $(document).trigger("custom:close");
+  
+  //if(riot.enableReloadContent){
+    setTimeout(function() {
+      $('content').addClass('not-opacity');
+      riot.mount('content', 'page-invite-user', {content: 'content'});
+      riot.update();
+    }, 400);
+  //}
+  riot.enableReloadContent = true;
+  
+})
+
+riot.route(function(tagName) {
+
+  if(riot.enableFadeIn) $('content').removeClass('not-opacity');
+
+  riot.enableFadeIn = true;
+
+  if(tagName == 'login'){
+    $(document).trigger("custom:close");
+    setTimeout(function() {
+      $('content').addClass('not-opacity');
+      riot.mount('content', 'page-login', {content: 'content'});
+      riot.update();
+    }, 400);
+  }else{
+    window.location.href = './#login';
+  }
+  
 });
 
 riot.route.start(true);
